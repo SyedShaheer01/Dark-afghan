f8175ed0571ff654b1fc83e1ef83fe09aef3d9a3
shaheer <syedshaheer266@gmail.com>
2025-09-20 16:54:37 +0500

    first commit

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 document.querySelectorAll('.img-box').forEach(box => {
   box.addEventListener('click', () => {
-    // pehle sab box se active hatao
     document.querySelectorAll('.img-box').forEach(b => b.classList.remove('active'));
-    // jis box pe click hua usme active class lagao
     box.classList.add('active');
   });
 });
+
+
